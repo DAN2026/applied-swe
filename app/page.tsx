c2a15e64f9beb77b507be37bbe3c2d45186bba0a
DAN2025 <danuniversity.education@outlook.com>
2025-12-06 22:39:49 +0000

    Attempting to resolve conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/general/LandingNavbar";
-import NavbarWrapper from "@/components/wrappers/NavbarWrapper";
-import prisma from "@/lib/prisma";
+// import Navbar from "@/components/general/LandingNavbar";
+// import NavbarWrapper from "@/components/wrappers/NavbarWrapper";
+// import prisma from "@/lib/prisma";
 
-export default function Landing(){
-    return (
-    <div className="h-[2000px]">
-        <NavbarWrapper></NavbarWrapper>
-    </div>
-    )
-}
+// export default function Landing(){
+//     return (
+//     <div className="h-[2000px]">
+//         <NavbarWrapper></NavbarWrapper>
+//     </div>
+//     )
+// }
